1fec0d4dddfaea7a9dbd12add490aba2f6825ec2
Juanpod <56892804+Juanpod@users.noreply.github.com>
2022-09-24 20:18:42 -0300

    Desafio 3

---
+++ Desafio_3/server.js
@@ -0,0 +1,25 @@
+const {Contenedor} = require("./clase") ;
+const express = require("express");
+
+const contenedor = new Contenedor("productos")
+
+//Crear el servidor
+const app = express();
+
+//Configurar las rutas
+app.get("/productos", async (req, res)=>{
+    let getAll = await contenedor.getAll()
+    res.send(getAll)
+})
+app.get("/productRandom", async (req, res)=>{
+    let getAll = await contenedor.getAll();
+    const randomId =  parseInt(Math.random()*getAll.length+1);
+    let producto = await contenedor.getById(randomId);
+    res.send(producto)
+})
+
+// Levantando el servidor
+app.listen(8080,()=>{
+    console.log("Server up")
+})
+
